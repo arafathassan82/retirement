b3405aa4900361c7ef618e7a7738c0e7d5847253
Spencer Nauman <snauman817@gmail.com>
2019-11-04 11:04:12 -0500

    Creates connection file

---
+++ database/db.php
@@ -0,0 +1,8 @@
+<?php
+$dbServerName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "retirement";
+
+$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
+?>
