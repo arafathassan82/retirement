289ff9366d5641a72fd5cc907716063f9aae63dd
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-05 11:04:21 -0500

    laid out admins_report, caregivers_home, family_home, and payment pages' html

---
+++ admins_report.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin's Report</title>
+  </head>
+  <body>
+    <h1>Admin's Report</h1>
+    <form method="post">
+      Date: <input type="date" name="date">
+      <input type="submit" name="submit" value="submit">
+    </form>
+
+    <!-- table displaying relevant data only for aa certain date (submitted by input above) for anything that has a false in it -->
+  </body>
+</html>
--- doctors_home.php
+++ doctors_home.php
@@ -12,5 +12,9 @@
       <input type="submit" name="upcoming appointments">
     </form>
     <!-- table that displays the patients and the date of appointments for aappointments between the dates of today and inputted date -->
+
+    <!-- each of the table results can be clicked, leading to a page with more information about that appointment
+         built using something similar to the flask stuff we did before. Pages aare specific to that appointment
+         page accessed by this link is shown on page 13 of the slides-->
   </body>
 </html>
