e434980f72fa86aff29c7b427f28a37dcbfb603a
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-05 10:13:13 -0500

    laid out registration_approval and patients pages

---
+++ patients.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Patients</title>
+  </head>
+  <body>
+    <h1>Patients</h1>
+    <!-- table which displays all patients, and a search option for each column -->
+  </body>
+</html>
---
+++ registration_approval.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registration Approval</title>
+  </head>
+  <body>
+    <h1>Registration Approval</h1>
+    <!-- a table with the name and role of all unapproved registrations, and an option yes/no whether to approve either of them -->
+  </body>
+</html>
