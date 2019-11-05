1ad201adeb1e8774b7603cb0731082da7a8dddab
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-05 09:04:10 -0500

    filled out layout of role page

---
+++ role/role.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Role</title>
+  </head>
+  <body>
+    <h1>Role</h1>
+    <!-- table displaying all roles and their access levels -->
+
+    <form method="post">
+      New Role: <input type="text" name="new_role">
+      Access Level: <input type="text" name="access_level">
+
+      <input type="submit" value="Ok" name="role">
+    </form>
+  </body>
+</html>
