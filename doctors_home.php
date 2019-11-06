712cc8bc3dac8fb6f5a09f16b1186f47f9693ad8
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-06 10:19:02 -0500

    added nav in includes folder

--- doctors_home.php
+++ doctors_home.php
@@ -1,3 +1,7 @@
+<?php
+  include "includes/nav.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
---
+++ includes/nav.php
@@ -0,0 +1,4 @@
+<nav>
+  <a href="~/employee.php">Employee</a>
+  <a href="~/login/login.html">Login</a>
+</nav>
