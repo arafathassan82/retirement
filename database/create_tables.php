1fa1be1ba61ba74474f0236202c53dcc18269ec1
Spencer Nauman <snauman817@gmail.com>
2019-11-06 08:57:33 -0500

    data insertion script finished

--- database/create_tables.php
+++ database/create_tables.php
@@ -15,7 +15,7 @@ $result = mysqli_query($conn, $query);
 $query2 = "CREATE TABLE `Users` (id bigint PRIMARY KEY AUTO_INCREMENT, roleid bigint REFERENCES Roles(id), fname varchar(30), lname varchar(30), email varchar(50), phone varchar(15), `password` text, dateofbirth date, familycode varchar(30), emergencycontact varchar(30), emergencyrelation varchar(20), approved boolean, `group` smallint);";
 $result2 = mysqli_query($conn, $query2);
 
-$query3 = "CREATE TABLE `Roster` (id bigint PRIMARY KEY AUTO_INCREMENT, date date, doctorid bigint REFERENCES Users (id));";
+$query3 = "CREATE TABLE `Roster` (id bigint PRIMARY KEY AUTO_INCREMENT, date date, doctorid bigint REFERENCES Users (id), supervisorid bigint REFERENCES Users (id), caregiver1id bigint REFERENCES Users (id), caregiver2id bigint REFERENCES Users (id), caregiver3id bigint REFERENCES Users (id), caregiver4id bigint REFERENCES Users (id));";
 $result3 = mysqli_query($conn, $query3);
 
 $query4 = "CREATE TABLE `Appointments` (id bigint PRIMARY KEY AUTO_INCREMENT, patientid bigint REFERENCES Users (id), date date, morning varchar(30), afternoon varchar(30), night varchar(30));";
