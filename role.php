c5b271dc072bb32fcfe0ebb12b8925ae2ba8aa99
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-11 08:51:19 -0500

    finished core functionalities of role page, including a table that shows all roles, and a form to insert new roles

--- role.php
+++ role.php
@@ -1,3 +1,15 @@
+<?php
+  $root = $_SERVER['DOCUMENT_ROOT'];
+  include "$root/retirement-home/database/db.php";
+
+
+  if (isset($_POST['role'])) {
+    $new_role = $_POST['new_role'];
+    $access_level = $_POST['access_level'];
+    $query = "INSERT INTO `Roles` (`name`, `accesslevel`) VALUES ('$new_role', $access_level);";
+    mysqli_query($conn, $query);
+  }
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -15,6 +27,27 @@
     ?>
     <h1>Role</h1>
     <!-- table displaying all roles and their access levels -->
+    <table>
+      <tr>
+        <th>Role</th>
+        <th>Access Level</th>
+      </tr>
+    <?php
+      $sql = "SELECT name, accesslevel FROM roles;";
+      $result = mysqli_query($conn, $sql);
+      $resultCheck = mysqli_num_rows($result);
+      if($resultCheck>0) {
+        while($row = mysqli_fetch_assoc($result)) {
+          $role = $row['name'];
+          $access = $row['accesslevel'];
+          echo "<tr>";
+          echo "<td>$role</td>";
+          echo "<td>$access</td>";
+          echo "</tr>";
+        }
+      }
+    ?>
+    </table>
 
     <form method="post">
       New Role: <input type="text" name="new_role">
