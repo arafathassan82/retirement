2f2ee5e5c26558c8f9f96fee1b465416f1b96197
Spencer Nauman <snauman817@gmail.com>
2019-11-11 10:03:33 -0500

    fixes bugs with verification

--- disapprove.php
+++ disapprove.php
@@ -1,9 +1,17 @@
 <?php
-include "database/db.php";
-$userid = $_GET['id'];
+session_start();
+// always include session_start in pages that you want to reference session variables in.
+if ($_SESSION['role'] != 1) {
+  header("Location: login.php");
+} else {
 
-$sql = "DELETE FROM `Users` WHERE id = $userid;";
-mysqli_query($conn, $sql);
+    if(isset($_GET['id'])){
+        include_once "database/db.php";
+        $userid = $_GET['id'];
 
-header("Location: registration_approval.php")
+        $sql = "DELETE FROM `Users` WHERE id = $userid AND approved = 0;";
+        mysqli_query($conn, $sql);
+    }
+    header("Location: registration_approval.php");
+}
 ?>
