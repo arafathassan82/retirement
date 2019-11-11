c163596ac4ebfade451f4ef256f1f7bf058ca7f3
Spencer Nauman <snauman817@gmail.com>
2019-11-11 07:45:36 -0500

    query lists unapproved individuals in table

--- registration_approval.php
+++ registration_approval.php
@@ -15,5 +15,39 @@
     ?>
     <h1>Registration Approval</h1>
     <!-- a table with the name and role of all unapproved registrations, and an option yes/no whether to approve either of them -->
+    
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Role</th>
+        <th>Approve</th>
+        <th>Disapprove</th>
+      </tr>
+      <?php
+        include_once "database/db.php";
+
+        $sql = "SELECT Users.id, roleid, name, fname, lname, approved FROM `Users`
+        JOIN `Roles` ON Users.roleid = Roles.id
+        WHERE approved = 0;";
+        $results = mysqli_query($conn, $sql);
+
+        if($results){
+          while($row = mysqli_fetch_assoc($results)){
+            $role = $row['name'];
+            $fname = $row['fname'];
+            $lname = $row['lname'];
+            $approved = $row['approved'];
+
+            echo "<tr>
+              <td>$fname $lname</td>
+              <td>$role</td>
+              <td></td>
+              <td></td>
+            </tr>";
+          }
+        }
+
+      ?>
+    </table>
   </body>
 </html>
