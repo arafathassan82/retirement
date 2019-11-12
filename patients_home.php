aee870a3a177f878cacf597438987efb53f4b609
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-12 10:21:53 -0500

    finished core functionalities of the patients home page, including a table that displays the status of meals and medicine on a certain day

--- patients_home.php
+++ patients_home.php
@@ -34,9 +34,68 @@
         echo "<p>Name: $fname $lname</p>";
       ?>
       <!-- should be filled in with today's date by default -->
-      <input type="date" name="date">
+      <input type="date" name="date" value="<?php echo date('Y-m-d');?>">
       <input type="submit" name="submit">
     </form>
+
+    <!-- This is where we put the table result from the post query (should also spin up using the current date if no post was sent) -->
+    <table>
+      <tr>
+        <th>Morning</th>
+        <th>Afternoon</th>
+        <th>Night</th>
+        <th>Breakfast</th>
+        <th>Lunch</th>
+        <th>Dinner</th>
+      </tr>
+    <?php
+      if (isset($_POST['submit'])) {
+        $date = $_POST['date'];
+      } else {
+        $date = date('Y-m-d');
+      }
+
+      $reports_query = "SELECT * FROM Reports r WHERE r.date = '$date' AND r.patientid = {$_SESSION['user']};";
+      $result = mysqli_query($conn, $reports_query);
+      $resultCheck = mysqli_num_rows($result);
+      if($resultCheck>0) {
+        while($row = mysqli_fetch_assoc($result)) {
+          echo "<tr>";
+          if ($row['morning'] == 1) {
+            echo "<td><input type='checkbox' checked onclick='return false;'></td>";
+          } else {
+            echo "<td><input type='checkbox' onclick='return false;'></td>";
+          }
+          if ($row['afternoon'] == 1) {
+            echo "<td><input type='checkbox' checked onclick='return false;'></td>";
+          } else {
+            echo "<td><input type='checkbox' onclick='return false;'></td>";
+          }
+          if ($row['night'] == 1) {
+            echo "<td><input type='checkbox' checked onclick='return false;'></td>";
+          } else {
+            echo "<td><input type='checkbox' onclick='return false;'></td>";
+          }
+          if ($row['breakfast'] == 1) {
+            echo "<td><input type='checkbox' checked onclick='return false;'></td>";
+          } else {
+            echo "<td><input type='checkbox' onclick='return false;'></td>";
+          }
+          if ($row['lunch'] == 1) {
+            echo "<td><input type='checkbox' checked onclick='return false;'></td>";
+          } else {
+            echo "<td><input type='checkbox' onclick='return false;'></td>";
+          }
+          if ($row['dinner'] == 1) {
+            echo "<td><input type='checkbox' checked onclick='return false;'></td>";
+          } else {
+            echo "<td><input type='checkbox' onclick='return false;'></td>";
+          }
+          echo "</tr>";
+        }
+      }
+    ?>
+    </table>
 
   </body>
 </html>
