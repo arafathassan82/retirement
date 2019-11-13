78f4f9e9ed2a70f0cd16cd1b4e0497ad92701ad4
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-13 09:12:43 -0500

    changed links leading to home pages to always lead to home.php

--- includes/nav.php
+++ includes/nav.php
@@ -11,20 +11,20 @@
   if(isset($_SESSION['role'])) {
 
     if ($_SESSION['role'] == 6) {
-      $nav_arr[] = 'patients_home';
+      $nav_arr[] = 'home';
       $nav_arr[] = 'roster';
     } else if ($_SESSION['role'] == 5) {
-      $nav_arr[] = 'family_home';
+      $nav_arr[] = 'home';
       $nav_arr[] = 'roster';
     } else if ($_SESSION['role'] == 4) {
-      $nav_arr[] = 'caregiver_home';
+      $nav_arr[] = 'home';
       $nav_arr[] = 'roster';
     } else if ($_SESSION['role'] == 3) {
       $nav_arr[] = 'home';
       $nav_arr[] = 'roster';
       $nav_arr[] = 'new_roster';
     } else if ($_SESSION['role'] == 2) {
-      $nav_arr[] = 'doctors_home';
+      $nav_arr[] = 'home';
       $nav_arr[] = 'roster';
     } else if ($_SESSION['role'] == 1) {
       $nav_arr[] = 'home';
