7f85b0d024d2d024f6fa1a92fb294cf90ab1d6dc
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-15 08:01:44 -0500

    finshed functionality of employees table, which displays aand allows searches for employees

--- employee.php
+++ employee.php
@@ -42,10 +42,10 @@
             <input type="text" name="lname_query">
           </td>
           <td>
-            <input type="number" name="role_query">
+            <input type="text" name="role_query">
           </td>
           <td>
-            <input type="text" name="salary_query">
+            <input type="number" name="salary_query">
           </td>
           <td>
             <input type="submit" value="submit">
@@ -56,7 +56,60 @@
         $root = $_SERVER['DOCUMENT_ROOT'];
         include "$root/retirement-home/database/db.php";
 
-        
+        $sql_query = "SELECT u.id, roleid, fname, lname, salary, name FROM Users u JOIN Roles r ON u.roleid = r.id WHERE ";
+        $sql_arr = array();
+        if(isset($_POST['id_query']) and $_POST['id_query'] != "") {
+          $sql_arr["id"] = $_POST['id_query'];
+        }
+        if(isset($_POST['fname_query']) and $_POST['fname_query'] != "") {
+          $sql_arr["fname"] = $_POST['fname_query'];
+        }
+        if(isset($_POST['lname_query']) and $_POST['lname_query'] != "") {
+          $sql_arr["lname"] = $_POST['lname_query'];
+        }
+        if(isset($_POST['role_query']) and $_POST['role_query'] != "") {
+          $sql_arr["name"] = $_POST['role_query'];
+        }
+        if(isset($_POST['salary_query']) and $_POST['salary_query'] != "") {
+          $sql_arr["salary"] = $_POST['salary_query'];
+        }
+
+        $count = 0;
+        foreach ($sql_arr as $key => $val) {
+          if ($count >= 1) {
+            if ($key == 'id' or $key == 'salary'){
+              $sql_query .= "AND u.$key = $val ";
+            } else {
+              $sql_query .= "AND $key = '$val' ";
+            }
+          } else {
+            if ($key == 'id' or $key == 'salary'){
+              $sql_query .= "u.$key = $val ";
+            } else {
+              $sql_query .= "$key = '$val' ";
+            }
+          }
+          $count += 1;
+        }
+        if($count > 0) {
+          $sql_query .= "AND roleid NOT IN (6, 5);";
+        } else {
+          $sql_query .= "roleid NOT IN (6, 5);";
+        }
+
+        $result = mysqli_query($conn, $sql_query);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck>0) {
+          while($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>{$row['id']}</td>";
+            echo "<td>{$row['fname']}</td>";
+            echo "<td>{$row['lname']}</td>";
+            echo "<td>{$row['name']}</td>";
+            echo "<td>{$row['salary']}</td>";
+            echo "</tr>";
+          }
+        }
       ?>
     </table>
   </body>
