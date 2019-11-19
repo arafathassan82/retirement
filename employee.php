b0dd9d07b446c8e8bf65e5e515c359709deda459
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-19 10:34:24 -0500

    fixed employees table to now update the employee's table salary, and join the employees table for the SELECT query

--- employee.php
+++ employee.php
@@ -57,11 +57,11 @@
         include "$root/retirement-home/database/db.php";
 
         if(isset($_POST['new_salary'])) {
-          $add_salary = "UPDATE Users SET salary = {$_POST['salary']} WHERE id = {$_POST['employee_id']};";
+          $add_salary = "UPDATE Employees SET salary = {$_POST['salary']} WHERE userid = {$_POST['employee_id']};";
           mysqli_query($conn, $add_salary);
         }
 
-        $sql_query = "SELECT u.id, roleid, fname, lname, salary, name FROM Users u JOIN Roles r ON u.roleid = r.id WHERE ";
+        $sql_query = "SELECT u.id, roleid, fname, lname, salary, name FROM Users u JOIN Roles r ON u.roleid = r.id JOIN Employees e ON u.id = e.userid WHERE ";
         $sql_arr = array();
         if(isset($_POST['id_query']) and $_POST['id_query'] != "") {
           $sql_arr["id"] = $_POST['id_query'];
