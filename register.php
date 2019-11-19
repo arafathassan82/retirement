829c40bc6389f0c43e650f7cc3a4b20f93affebe
Spencer Nauman <snauman817@gmail.com>
2019-11-19 11:10:12 -0500

    Register works for family members and employees

--- register.php
+++ register.php
@@ -13,6 +13,18 @@
       $sql2 = "INSERT INTO `Patients` (`userid`, `familycode`, `emergencycontact`, `emergencyrelation`, `group`, `due`, `admissiondate`)
       VALUES ({$row['id']}, '{$_POST['familycode']}', '{$_POST['emergency']}', '{$_POST['relation']}', 0, 0.0000, NULL);";
       mysqli_query($conn, $sql2);
+    } elseif($_POST['role'] != 5){
+      $sql = "INSERT INTO `Users` (`roleid`, `fname`, `lname`, `email`, `phone`, `password`, `dateofbirth`, `approved`)
+      VALUES ('{$_POST['role']}', '{$_POST['fName']}', '{$_POST['lName']}', '{$_POST['email']}', '{$_POST['phone']}', '{$_POST['password']}', '{$_POST['date']}', FALSE);";
+      mysqli_query($conn, $sql);
+
+      $get = "SELECT id FROM `Users` WHERE `roleid` = {$_POST['role']} AND `fname` = '{$_POST['fName']}' AND `lname` = '{$_POST['lName']}' AND `email` = '{$_POST['email']}' AND `phone` = '{$_POST['phone']}';";
+      $result = mysqli_query($conn, $get);
+      $row = mysqli_fetch_assoc($result);
+
+      $sql2 = "INSERT INTO `Employees` (`userid`, `salary`)
+      VALUES ({$row['id']}, 0);";
+      mysqli_query($conn, $sql2);
     } else {
       $sql = "INSERT INTO `Users` (`roleid`, `fname`, `lname`, `email`, `phone`, `password`, `dateofbirth`, `approved`)
       VALUES ('{$_POST['role']}', '{$_POST['fName']}', '{$_POST['lName']}', '{$_POST['email']}', '{$_POST['phone']}', '{$_POST['password']}', '{$_POST['date']}', FALSE);";
