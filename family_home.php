3e5ecb8f9695f708715a02ba19e9b25df24418cb
Spencer Nauman <snauman817@gmail.com>
2019-11-21 08:16:02 -0500

    Updates inputs on family_home table, creates new reports if the current day has no reports

--- family_home.php
+++ family_home.php
@@ -13,18 +13,133 @@
         if ($_SESSION['role'] != 5) {
           header("Location: home.php");
         }
+
+        include 'database/db.php';
+        $dailyreportscheck = "SELECT `date` FROM `Reports` ORDER BY `date` ASC;";
+        $result = mysqli_query($conn, $dailyreportscheck);
+        $resultCheck = mysqli_num_rows($result);
+
+        $currentdate = date('Y-m-d');
+        $row = mysqli_fetch_assoc($result);
+        if($row['date'] != $currentdate){
+          $patients = "SELECT userid FROM `Patients`;";
+          $patientsresult = mysqli_query($conn, $patients);
+          $resultCheckPatients = mysqli_num_rows($patientsresult);
+
+          if($resultCheckPatients > 0){
+            while($patientsRow = mysqli_fetch_assoc($patientsresult)){
+              $id = $patientsRow['userid'];
+
+              $reportsadd = "INSERT INTO `Reports` (`patientid`, `morning`, `afternoon`, `night`, `breakfast`, `lunch`, `dinner`, `date`)
+              VALUES ($id, 0, 0, 0, 0, 0, 0, '$currentdate');";
+              mysqli_query($conn, $reportsadd);
+            }
+          }
+        }
       }
     ?>
     <h1>Home</h1>
     <form method="post">
-      Date: <input type="date" name="date">
-      Family Code: <input type="number" name="family_code">
+      Date: <input type="date" name="date" value="<?php $date = date("Y-m-d"); echo $date ?>">
+      Family Code: <input type="text" name="family_code">
       Patient ID: <input type="number" name="patient_id">
       <input type="submit" value="Status" name="submit">
     </form>
 
     <!-- pull up a table representing the current status of a resident's medicine and meals
          based on the patient id, date and family code sent through in form -->
+    <?php
+      if(isset($_POST['family_code'])){
+        echo "<table>
+          <tr>
+            <th>Doctor's Name</th>
+            <th>Doctor's Appointment</th>
+            <th>Caregiver's Name</th>
+            <th>Morning Medicine</th>
+            <th>Afternoon Medicine</th>
+            <th>Night Medicine</th>
+            <th>Breakfast</th>
+            <th>Lunch</th>
+            <th>Dinner</th>
+          </tr>";
+
+        include "database/db.php";
+
+        $sql = "SELECT morning, afternoon, night, breakfast, lunch, dinner FROM `Patients`
+        JOIN `Reports` ON `Reports`.patientid = `Patients`.userid
+        JOIN `Roster` ON `Reports`.`date` = `Roster`.`date`
+        WHERE `Patients`.familycode = '{$_POST['family_code']}' OR `Patients`.userid = {$_POST['patient_id']};";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+
+        $sql2 = "SELECT isfinished, doctorid, `Users`.fname AS fname, `Users`.lname AS lname FROM `Appointments`
+        JOIN `Users` ON `Appointments`.doctorid = `Users`.id
+        WHERE patientid = {$_POST['patient_id']}
+        ORDER BY `date` ASC;";
+        $result2 = mysqli_query($conn, $sql2);
+        $resultCheck2 = mysqli_num_rows($result2);
+
+        
+        echo "<tr>";
+
+        if($resultCheck2 > 0){
+          $row2 = mysqli_fetch_assoc($result2);
+          echo "<td>{$row2['fname']} {$row2['lname']}</td>";
+          
+          if($row2['isfinished'] == 1){
+            echo "<td>✔️</td>";
+          } else {
+            echo "<td>❌</td>";
+          }
+        }
+        
+        echo "<td>Caregiver Name not yet implemented</td>";
+
+        if($resultCheck > 0){
+          while($row = mysqli_fetch_assoc($result)){    
+            if($row['morning'] == 1){
+              echo "<td>✔️</td>";
+            } else {
+              echo "<td>❌</td>";
+            }
+
+            if($row['afternoon'] == 1){
+              echo "<td>✔️</td>";
+            } else {
+              echo "<td>❌</td>";
+            }
+
+            if($row['night'] == 1){
+              echo "<td>✔️</td>";
+            } else {
+              echo "<td>❌</td>";
+            }
+
+            if($row['breakfast'] == 1){
+              echo "<td>✔️</td>";
+            } else {
+              echo "<td>❌</td>";
+            }
+
+            if($row['lunch'] == 1){
+              echo "<td>✔️</td>";
+            } else {
+              echo "<td>❌</td>";
+            }
+
+            if($row['dinner'] == 1){
+              echo "<td>✔️</td>";
+            } else {
+              echo "<td>❌</td>";
+            }
+
+            echo "</tr>";
+          }
+        }
+
+        echo "</table>";
+      }
+    ?>
 
   </body>
 </html>
