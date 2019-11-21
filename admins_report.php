c3c575fe397bee18e73eaac4b91b2a6dcca9fded
Lucas Nauman <Lnauman1700@gmail.com>
2019-11-21 09:23:40 -0500

    finished base query to display some of the infor required in admin's report

--- admins_report.php
+++ admins_report.php
@@ -19,6 +19,70 @@
       <input type="submit" name="submit" value="submit">
     </form>
 
+    <?php
+      $date = date('Y-m-d');
+      if (isset($_POST['submit'])) {
+        echo "<h2>Missed Patient Activity for {$_POST['date']}:</h2>";
+      } else {
+        echo "<h2>Missed Patient Activity for $date:</h2>";
+      }
+    ?>
+
     <!-- table displaying relevant data only for aa certain date (submitted by input above) for anything that has a false in it -->
+    <table>
+      <tr>
+        <th>Patient's Name</th>
+        <th>Morning Medicine</th>
+        <th>Afternoon Medicine</th>
+        <th>Night Medicine</th>
+        <th>Breakfast</th>
+        <th>Lunch</th>
+        <th>Dinner</th>
+      </tr>
+    <?php
+      $root = $_SERVER['DOCUMENT_ROOT'];
+      include "$root/retirement-home/database/db.php";
+
+      if (isset($_POST['submit'])) {
+        $sql = "SELECT * FROM Reports r JOIN Users u ON u.id = r.patientid WHERE date = '{$_POST['date']}' AND (morning != 1 OR afternoon != 1 OR night != 1 OR breakfast != 1 OR lunch != 1 OR dinner != 1);";
+
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck>0) {
+          while($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>{$row['fname']} {$row['lname']}</td>";
+            echo "<td>{$row['morning']}</td>";
+            echo "<td>{$row['afternoon']}</td>";
+            echo "<td>{$row['night']}</td>";
+            echo "<td>{$row['breakfast']}</td>";
+            echo "<td>{$row['lunch']}</td>";
+            echo "<td>{$row['dinner']}</td>";
+            echo "</tr>";
+          }
+        }
+      } else {
+        $date = date('Y-m-d');
+        $sql = "SELECT * FROM Reports r JOIN Users u ON u.id = r.patientid WHERE date = '$date' AND (morning != 1 OR afternoon != 1 OR night != 1 OR breakfast != 1 OR lunch != 1 OR dinner != 1);";
+
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck>0) {
+          while($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>{$row['fname']} {$row['lname']}</td>";
+            echo "<td>{$row['morning']}</td>";
+            echo "<td>{$row['afternoon']}</td>";
+            echo "<td>{$row['night']}</td>";
+            echo "<td>{$row['breakfast']}</td>";
+            echo "<td>{$row['lunch']}</td>";
+            echo "<td>{$row['dinner']}</td>";
+            echo "</tr>";
+          }
+        }
+      }
+      echo "</table>";
+    ?>
+    </table>
   </body>
 </html>
