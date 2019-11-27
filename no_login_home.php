4a2e17efd4641850cf00226e65c028e2c0ee8ef8
Spencer Nauman <snauman817@gmail.com>
2019-11-27 08:41:19 -0500

    No login home styled

--- no_login_home.php
+++ no_login_home.php
@@ -3,9 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="includes/styles.css">
   </head>
-  <body>
-    <a href="login.php">Login</a>
-    <a href="register.php">Register</a>
+  <body id="nologinhome">
+    <main>
+      <a href="login.php">Login</a>
+      <a href="register.php">Register</a>
+      <section id="bgimg"><img src="https://www.espritlifestyle.com/app/uploads/2018/05/DSC_1804-e1530027414598.jpg" alt="retirement home"></section>
+    </main>
   </body>
 </html>
